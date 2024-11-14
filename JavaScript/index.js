40bfae55568d8ae10ebf191d06c83b0c32cbef4e
ejima_xx <060812ez.tm@gmail.com>
2024-11-14 15:09:38 +0900

    ロード画面クラス化、jQuery脱却

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,73 +1,37 @@
+import { Loader } from "./util.js";
+
 //hamburgermenu
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 
 hamburger.addEventListener('click', () => {
-    toggleClasses();
+  hamburger.classList.toggle("open");
+  menu.classList.toggle("open");
 });
 
-function toggleClasses() {
-  hamburger.classList.toggle('open');
-    menu.classList.toggle('open');
+/** ロード画面 */
+const loader = new Loader(document.querySelector(".loader"));
+
+/** サイトに訪れたか。セッションストレージに保存される */ 
+const visited = sessionStorage.getItem('visited');
+
+// フラグがない場合（初回アクセス時）
+if (!visited) {
+  loader.setText(`
+    project name: Code from -zero-<br>
+    system loading... success<br>
+    let's start cording!
+  `).display(true).animate().fadeOut(3000);
+  
+  // セッションストレージにフラグを保存
+  sessionStorage.setItem('visited', true);
 }
-
-
-//loading window
-const element = document.querySelector(".loader");
-let fadeoutFlag = true;
-// セッションストレージからフラグを取得
-const isFirstLoad = sessionStorage.getItem('isFirstLoad');
-
+else {
+  loader.setText(`
+    system loading... success
+  `).display(true).animate().fadeOut(1200);
+}
 // ページの読み込みが完了したときに実行される関数
 document.addEventListener('DOMContentLoaded', function() {
-  // フラグがない場合（初回アクセス時）
-  if (!isFirstLoad) {
-    // 初回アクセス時の処理を記述
-    element.style = "display: block;";
-      // js-textというクラスを取得し、elementsという変数を宣言
-
-  // animateTextという関数を、elementsの文字数の回数繰り返す
-  // for (var i = 0; i < element.children[0].length; i++) {
-      animateText(element.children[0]);
-  // }
-    console.log('初回アクセスです');
-    
-    // セッションストレージにフラグを保存
-    sessionStorage.setItem('isFirstLoad', true);
-  } else {
-    // 2回目以降のアクセス時の処理を記述
-    console.log('2回目以降のアクセスです');
-    element.style = "display: none;";
-  }
+  
 });
-
-function animateText(element) {
-  // elementの文字情報のみをtextという変数に代入
-  let text = element.innerText;
-  element.innerText = "";
-
-  // textの一文字ずつをspanタグで囲う
-  for (var i = 0; i < text.length; i++) {
-      var span = document.createElement("span");
-      span.innerText = text[i];
-      element.appendChild(span);
-  }
-
-  // spanで囲った文字をspansに代入
-  let spans = element.getElementsByTagName("span");
-  // spansのstyleをopacity=1にする処理を、文字数分繰り返す
-  for (var j = 0; j < spans.length; j++) {
-      (function (index) {
-          setTimeout(function () {
-              spans[index].style.opacity = 1;
-        }, index * 30); // 遅延時間を調整
-      })(j);
-  }
-}
-function FadeoutWindow(){
-  $('.loader').fadeOut(800);
-  fadeoutFlag = false;
-}
-window.onload=function(){
-  setTimeout(function(){FadeoutWindow();},3000)
-}
