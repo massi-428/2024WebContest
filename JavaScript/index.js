a5ab189750de3d6355889d4cc951517344abca04
ejima_xx <060812ez.tm@gmail.com>
2024-11-19 11:37:43 +0900

    ホームページ改修

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,37 +1,53 @@
 import { Loader } from "./util.js";
 
-//hamburgermenu
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle("open");
-  menu.classList.toggle("open");
-});
-
 /** ロード画面 */
 const loader = new Loader(document.querySelector(".loader"));
 
-/** サイトに訪れたか。セッションストレージに保存される */ 
+/** サイトに訪れたか。セッションストレージに保存される */
 const visited = sessionStorage.getItem('visited');
 
 // フラグがない場合（初回アクセス時）
 if (!visited) {
-  loader.setText(
-    `project name: Code from -zero-
-     system loading... success
-     let's start cording!`
-  ).display(true).animate().fadeOut(3000);
-  
-  // セッションストレージにフラグを保存
-  sessionStorage.setItem('visited', true);
+	loader.setText(
+		`project name: Code from -zero-
+		 system loading... success
+		 let's start cording!`
+	).display(true).animate().fadeOut(3000);
+
+	// セッションストレージにフラグを保存
+	sessionStorage.setItem('visited', true);
 }
 else {
-  loader.setText(
-    `system loading... success`
-  ).display(true).animate().fadeOut(1200);
+	loader.setText(
+		`system loading... success`
+	).display(true).animate().fadeOut(1200);
 }
+
+/** メニュー */
+const menu = document.querySelector('#menu');
+
+// メニューを開く
+document.getElementById("menuOpen").addEventListener("click", () => {
+	menu.classList.add("active");
+})
+
+// メニューを閉じる
+document.getElementById("menuClose").addEventListener("click", () => {
+	menu.classList.remove("active");
+})
+
+// ヘッダーの開閉トリガー
+window.addEventListener("scroll", () => {
+	const header = document.querySelector("header");
+	if (window.scrollY >= document.querySelector("section").offsetTop) {
+		header.classList.add("active");
+	}
+	else if (header.classList.contains("active") && window.scrollY === 0) {
+		header.classList.remove("active");
+	}
+})
+
 // ページの読み込みが完了したときに実行される関数
-document.addEventListener('DOMContentLoaded', function() {
-  
+document.addEventListener('DOMContentLoaded', function () {
+	
 });
